0888529b0264cd6b447b9a9aa4ba4acfe3b90632
ali-r-glitch <aliraffee2@gmail.com>
2025-04-07 14:35:51 +0200

    updated errors and READ ME

--- getbest.py
+++ getbest.py
@@ -5,10 +5,14 @@ import sys
 def getCols(f):
     ''' Identify the columns that contain the marks and student numbers '''
     headings = f.readline().strip().split(",")
-    i=1
+    i=2
     for head in headings:
-        if head == "Student Number": num_col=i
-        elif head == "Mark" : mark_col = i
+        if head == "Student Number": 
+                num_col=i
+                mark_col=i+1
+        elif head == "Mark" : 
+                  mark_col = i
+                  num_col=i-1
     return (num_col, mark_col)
 
 def findTop(f,num_col, mark_col):
@@ -17,8 +21,10 @@ def findTop(f,num_col, mark_col):
     for line in f:
         data = line.strip().split(",")
         mark = int(data[mark_col])
+        name = int(data[num_col])
         if mark > best:
             best=mark
+            best_idx=name
     return best_idx, best
 
 f = open(sys.argv[1])
